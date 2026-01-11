ed50cc94d085d106318b58f8b3a9233c5848ea5a
tovam <tovam@users.noreply.github.com>
2026-01-11 17:20:44 +0100

    Align packaging scripts with fork app name

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 # INSTALL xcpretty: sudo gem install xcpretty
 
-NAME='MTMR'
+SCHEME='MTMR'
+APP_NAME='MTMR tovam'
 
 rm -r Release 2>/dev/null
 
 xcodebuild archive \
-	-scheme "$NAME" \
+	-scheme "$SCHEME" \
 	-archivePath Release/App.xcarchive | xcpretty -c
 
 xcodebuild \
@@ -18,17 +19,19 @@ cd Release
 rm -r App.xcarchive
 
 # Prerequisite: npm i -g create-dmg
-NAME_DMG="${NAME}.app"
-echo $NAME_DMG
-create-dmg $NAME_DMG
+APP_BUNDLE="${APP_NAME}.app"
+echo $APP_BUNDLE
+create-dmg "$APP_BUNDLE"
 
 DATE=`LC_ALL=en_US.utf8 date +"%a, %d %b %Y %H:%M:%S %z"`
-BUILD=`/usr/libexec/PlistBuddy -c "Print CFBundleVersion" ${NAME}.app/Contents/Info.plist`
-VERSION=`/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" ${NAME}.app/Contents/Info.plist`
-MINIMUM=`/usr/libexec/PlistBuddy -c "Print LSMinimumSystemVersion" ${NAME}.app/Contents/Info.plist`
-SIZE=`stat -f%z MTMR\ ${VERSION}.dmg`
-SIGN=`~/Sparkle/bin/sign_update MTMR\ ${VERSION}.dmg ~/Sparkle/bin/dsa_priv.pem | awk '{printf "%s",$0} END {print ""}'`
-SHA256=`shasum -a 256 MTMR\ ${VERSION}.dmg | awk '{print $1}'`
+BUILD=`/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${APP_BUNDLE}/Contents/Info.plist"`
+VERSION=`/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "${APP_BUNDLE}/Contents/Info.plist"`
+MINIMUM=`/usr/libexec/PlistBuddy -c "Print LSMinimumSystemVersion" "${APP_BUNDLE}/Contents/Info.plist"`
+DMG_NAME="${APP_NAME} ${VERSION}.dmg"
+DMG_URL_NAME=`printf "%s" "$DMG_NAME" | sed 's/ /%20/g'`
+SIZE=`stat -f%z "$DMG_NAME"`
+SIGN=`~/Sparkle/bin/sign_update "$DMG_NAME" ~/Sparkle/bin/dsa_priv.pem | awk '{printf "%s",$0} END {print ""}'`
+SHA256=`shasum -a 256 "$DMG_NAME" | awk '{print $1}'`
 
 # ditto -c -k --sequesterRsrc --keepParent "${NAME}.app" "${NAME}v${VERSION}.zip"
 
@@ -49,7 +52,7 @@ echo "<?xml version=\"1.0\" standalone=\"yes\"?>
 				${1}
 			</description>
 			<sparkle:minimumSystemVersion>${MINIMUM}</sparkle:minimumSystemVersion>
-			<enclosure url=\"https://mtmr.app/MTMR%20${VERSION}.dmg\"
+			<enclosure url=\"https://mtmr.app/${DMG_URL_NAME}\"
 				sparkle:version=\"${BUILD}\"
 				sparkle:shortVersionString=\"${VERSION}\"
 				length=\"${SIZE}\"
@@ -68,5 +71,5 @@ echo "  sha256 \"${SHA256}\""
 echo ""
 echo "Update MTMR v${VERSION}"
 
-scp MTMR\ ${VERSION}.dmg do:/var/www/mtmr
+scp "$DMG_NAME" do:/var/www/mtmr
 scp appcast.xml do:/var/www/mtmr
