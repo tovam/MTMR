487a8588bafe934b2c5225462f7fe85f2a25eeb3
tovam <tovam@users.noreply.github.com>
2026-09-07 23:39:07 +0200

    Remove app-only assertion from legacy test target

--- MTMRTests/BackgroundColorTests.swift
+++ MTMRTests/BackgroundColorTests.swift
@@ -1,23 +1,6 @@
 import AppKit
 import XCTest
 
-private final class RuntimeVisualChangeCounter: @unchecked Sendable {
-    private let lock = NSLock()
-    private var storedValue = 0
-
-    func increment() {
-        lock.lock()
-        storedValue += 1
-        lock.unlock()
-    }
-
-    var value: Int {
-        lock.lock()
-        defer { lock.unlock() }
-        return storedValue
-    }
-}
-
 class BackgroundColorTests: XCTestCase {
     func testOpaque() {
         let buttonNoActionFixture = """
@@ -54,34 +37,5 @@ class BackgroundColorTests: XCTestCase {
         let image = NSImage(size: NSSize(width: 24, height: 24))
 
         XCTAssertNil(image.resize(maxSize: NSSize(width: 24, height: 24)))
-    }
-
-    @MainActor
-    func testButtonPublishesOnlyRealVisualChanges() {
-        let button = CustomButtonTouchBarItem(identifier: .init("test.visual"), title: "A")
-        let counter = RuntimeVisualChangeCounter()
-        let observer = NotificationCenter.default.addObserver(
-            forName: .mmtmrRuntimeVisualDidChange,
-            object: nil,
-            queue: nil
-        ) { _ in
-            counter.increment()
-        }
-        defer { NotificationCenter.default.removeObserver(observer) }
-
-        button.title = "A"
-        button.image = nil
-        button.isBordered = true
-        XCTAssertEqual(counter.value, 0)
-
-        button.title = "B"
-        button.title = "B"
-        let image = NSImage(size: NSSize(width: 16, height: 16))
-        button.image = image
-        button.image = image
-        button.isBordered = false
-        button.isBordered = false
-
-        XCTAssertEqual(counter.value, 3)
     }
 }
